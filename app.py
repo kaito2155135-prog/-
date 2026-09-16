ff8fbaada9c8d5d574f9c505c57273f60418779e
kaito2155135-prog <kaito2155135@gmail.com>
2026-09-17 07:18:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2068,25 +2068,25 @@ def run_integrated_simulation(
                     derived_times
                 )
 
-val_to_use = float(
-    np.median(
-        sorted_times
-    )
-)
+                val_to_use = float(
+                    np.median(
+                        sorted_times
+                    )
+                )
 
-st.write(
-    "予測タイム確認:",
-    h_name,
-    "換算値=",
-    [round(x, 1) for x in sorted_times],
-    "中央値=",
-    round(val_to_use, 1),
-    "今回基準=",
-    round(target_base_seconds, 1)
-)
+                st.write(
+                    "予測タイム確認:",
+                    h_name,
+                    "換算値=",
+                    [round(x, 1) for x in sorted_times],
+                    "中央値=",
+                    round(val_to_use, 1),
+                    "今回基準=",
+                    round(target_base_seconds, 1)
+                )
 
-# 過去6走を今回条件へ換算した中央値を保存
-horse_predicted_time_map[h_name] = val_to_use
+                # 過去6走を今回条件へ換算した中央値を保存
+                horse_predicted_time_map[h_name] = val_to_use
 
                 time_advantage = (
                     target_base_seconds
@@ -2096,10 +2096,11 @@ horse_predicted_time_map[h_name] = val_to_use
                 soha_score = max(
                     -5.0,
                     min(
-                    12.0,
-                    time_advantage * 3.0
+                        12.0,
+                        time_advantage * 3.0
                     )
                 )
+
             # =============================================
             # 上がり3F指数
             # =============================================
