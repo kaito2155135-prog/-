f8caca10da562fade908ebbcffaaf7aecc6a39fc
kaito2155135-prog <kaito2155135@gmail.com>
2026-09-17 20:37:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2043,14 +2043,11 @@ def run_integrated_simulation(
                     if is_rising:
                         converted_time -= 0.2
 
-    if (
-    not pd.isna(converted_time)
-    and converted_time > 0
-):
-    derived_times.append(converted_time)                
-                    derived_times.append(
-                        converted_time
-                    )
+                    if (
+                    not pd.isna(converted_time)
+                    and converted_time > 0
+                ):
+                    derived_times.append(converted_time)                
 
                 # =========================================
                 # 上がり3F
