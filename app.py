4966fe1a34ec2f1dd4cc247da892e7332e65c992
kaito2155135-prog <kaito2155135@gmail.com>
2026-09-18 08:22:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -500,11 +500,10 @@ surface_condition_code = (
 
 default_baba = condition_from_code(surface_condition_code)
 
-st.write("脚質デバッグ:", h)
-
 race_rows = []
 
 for h in horses:
+    st.write("脚質デバッグ:", h)
     row = {
         "枠番": h.get("枠番", h.get("wakuban", 1)),
         "馬番": h.get("馬番", h.get("umaban", 1)),
